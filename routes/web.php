8f108f0f74877aa139b05e7693ca37c6a3b1613d
omid <omidkh7900@gamil.com>
2020-12-07 08:08:58 +0330

    install laravel/ui and authentications

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+\Illuminate\Support\Facades\Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
